99f82555d5f6019f743b928f944f70f8cdb22c30
Heikki Aittokumpu <heikkiaittokumpu@kamk.fi>
2026-01-09 19:14:40 +0200

    fix(ui): ensure reliable window dragging by passing clicks to header

--- src/lib/components/Hud.svelte
+++ src/lib/components/Hud.svelte
@@ -41,6 +41,7 @@
             fill="none"
             stroke="currentColor"
             stroke-width="2"
+            style="pointer-events: none;"
           >
             <polygon
               points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
@@ -135,6 +136,7 @@
     align-items: center;
     gap: 0.5rem;
     margin: 0;
+    pointer-events: none;
   }
 
   .title-icon {
